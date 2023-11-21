c45eeef463cd042b11b13d4ae9f00f0009b66577
arrazan7 <fayyadharrazanmiftakhul@mail.ugm.ac.id>
2023-11-21 20:28:04 +0700

    Commit pada 21 November 2023 pukul 20:30

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InfoController;
 use App\Http\Controllers\GreetController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\GalleryControllerAPI;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +24,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/info', [InfoController::class, 'index'])->name('info');
 Route::get('/greet', [GreetController::class, 'greet'])->name('greet');
+Route::post('/gallery', [GalleryControllerAPI::class, 'store'])->name('gallery.store');
+Route::get('/gallery', [GalleryControllerAPI::class, 'index'])->name('gallery.index');
+Route::get('/getgallery', [GalleryControllerAPI::class, 'get'])->name('gallery.get');
